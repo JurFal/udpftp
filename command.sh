a91f0deafbf912591f999b054f4b136faedb4c1a
JurFal <zyc12241252@163.com>
2025-11-04 03:30:03 +0800

    test: download tests

---
+++ command.sh
@@ -0,0 +1,1 @@
+python3 udpftp_client.py --server-host <server-host> --server-port <server-port> --op <download/upload> --file ./<client-file-name> --remote-name <server-file-name> --algo <sr/gbn> --cc <reno/vegas>
